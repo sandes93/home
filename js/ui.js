e13b3fc532546bfd345709f7b44a04028755c568
Sandesh Pathak <sandeshpathak0112@gmail.com>
2016-11-25 09:27:25 -0500

    initiate

---
+++ js/ui.js
@@ -0,0 +1,49 @@
+$('#fixed-nav').hide();
+
+$(window).scroll(function() {
+    if ($(window).scrollTop() > 100) {
+        $('#fixed-nav').fadeIn(200, function () {
+          $(this).show();
+        })
+    } else {
+        $('#fixed-nav').fadeOut(200, function () {
+          $(this).hide();
+        })
+    }
+});
+
+var counter = 0, // to keep track of current slide
+    $items = $('.diy-slideshow figure'), // a collection of all of the slides, caching for performance
+    numItems = $items.length; // total number of slides
+
+// this function is what cycles the slides, showing the next or previous slide and hiding all the others
+var showCurrent = function(){
+    var itemToShow = Math.abs(counter%numItems);// uses remainder (aka modulo) operator to get the actual index of the element to show  
+   
+  $items.removeClass('show'); // remove .show from whichever element currently has it
+  $items.eq(itemToShow).addClass('show');    
+};
+
+// add click events to prev & next buttons 
+$('.next').on('click', function(){
+    counter++;
+    showCurrent(); 
+});
+$('.prev').on('click', function(){
+    counter--;
+    showCurrent(); 
+});
+
+// if touch events are supported then add swipe interactions using TouchSwipe https://github.com/mattbryson/TouchSwipe-Jquery-Plugin
+if('ontouchstart' in window){
+  $('.diy-slideshow').swipe({
+    swipeLeft:function() {
+      counter++;
+      showCurrent(); 
+    },
+    swipeRight:function() {
+      counter--;
+      showCurrent(); 
+    }
+  });
+}
