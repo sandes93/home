e4343cbeee19eb5f602c13d2f78e03ef25bf04ba
Sandesh Pathak <sandeshpathak0112@gmail.com>
2016-12-19 11:10:18 -0500

    fixed slide scroll issue

--- js/ui.js
+++ js/ui.js
@@ -4,10 +4,15 @@ $(window).scroll(function() {
     if ($(window).scrollTop() > 100) {
         $('#fixed-nav').fadeIn(200, function () {
           $(this).show();
+          $('.prev').css({"z-index":"-1"});
+          $('.next').css({"z-index":"-1"});
+
         })
     } else {
         $('#fixed-nav').fadeOut(200, function () {
           $(this).hide();
+          $('.prev').css({"z-index":"1"});
+          $('.next').css({"z-index":"1"});
         })
     }
 });
